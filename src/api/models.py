62dc3fc5f811cd10b0f5cbd7e80f8fc1a3510cac
CarlosAguayo1 <carlosibanezaguayo@gmail.com>
2025-08-11 10:07:16 +0000

    proyecto final

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,73 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer, Float, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
+    password_hash: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    name:Mapped[str] = mapped_column(String(80), nullable=False)
+    level:Mapped[int] = mapped_column(Integer, default=1)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    registrations: Mapped[list["EventPlayer"]] = relationship("EventPlayer", back_populates="user", cascade="all, delete-orphan")
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "name" : self.name,
+            "level" : self.level,
+            "created_at" : self.created_at.isoformat(),
             # do not serialize the password, its a security breach
+        }
+    
+class Event(db.Model):
+    __tablename__="events"
+
+    id:Mapped[int] = mapped_column(Integer,primary_key=True)
+    sport: Mapped[str] = mapped_column(String(50), nullable=False),
+    datetime: Mapped[DateTime] = mapped_column(DateTime, nullable=False),
+    lat: Mapped[float] = mapped_column(Float, nullable=False),
+    lng: Mapped[float] = mapped_column(Float, nullable=False),
+    capacity: Mapped[int] = mapped_column(Integer, nullable=False),
+    price: Mapped[int] = mapped_column(Integer, default=0)
+    is_free: Mapped[bool] = mapped_column(Boolean, default=True)
+    players: Mapped[list["EventPlayer"]] = relationship("EventPlayer", back_populates="event", cascade="all, delete-orphan")
+
+def serialize(self):
+    return {
+        "id": self.id,
+        "sport": self.sport,
+        "datetime": self.datetime.isoformat(),
+        "lat": self.lat,
+        "lng": self.lng,
+        "capacity": self.capacity,
+        "price": self.price,
+        "is_free": self.is_free,
+    }
+
+class EventPlayer(db.Model):
+    __tablename__= "event_players"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    useer_id: Mapped[int] =mapped_column(Integer, ForeignKey("events.id"), nullable=False)
+    event_id: Mapped[int] =mapped_column(Integer, ForeignKey("events.id"), nullable=False)
+    paid: Mapped[bool] =mapped_column(Boolean, default=False)
+    material: Mapped[str | None] = mapped_column(String(200), nullable=True)
+    created_at: Mapped[datetime] =mapped_column(DateTime, default=datetime.utcnow)
+    user: Mapped["User"] = relationship("User", back_populates="registrations")
+    event: Mapped["Event"] = relationship("Event", back_populates="players")
+ 
+    def serialize(self):
+        return {
+            "id":self.id,
+            "user_id":self.user_id,
+            "event_id":self.event_id,
+            "paid":self.paid,
+            "material":self.material,
+            "created_at": self.created_at.isoformat(), 
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,13 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import (
+    create_access_token,
+    jwt_required,
+    get_jwt_identity
+)
+from .models import Event, EventPlayer
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +19,166 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/auth/register', methods=['POST'])
+def register():
+    data = request.get_json() or {}
+    email = data.get("email")
+    pasword = data.get("pasword")
+    name = data.get("name")
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    if not email or not pasword or not name:
+        raise APIException("Faltan campos obligatorios", status_code=400)
+    
+    if User.query.filter.by(email=email).first():
+        raise APIException("Email ya registrado", status_code=409)
+    
+    pw_hash = generate_password_hash(pasword)
+    user = User(email=email, pasword_hash=pw_hash, name=name)
+    db.session.add(user)
+    db.session.commit()
 
-    return jsonify(response_body), 200
+    return jsonify({"msg": "Usuario creado correctamente"}), 201
+
+@api.route('/auth/login', methods=['POST'])
+def login():
+
+    data = request.get.json() or {}
+    email = data.get("email")
+    password = data.get("password")
+
+    user = user.query.filter_by(email=email).first()
+    if not user or not check_password_hash(user.password_hash, password):
+        raise APIException("credenciales inválidas", status_code=401)
+    
+    token = create_access_token(identity=user.id)
+    return jsonify({"access_token": token}), 200
+
+@api.route('/users/me',methods=['GET'])
+@jwt_required()
+def get_profile():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    if not user:
+        raise APIException("Usuario no encontrado", status_code=404)
+    return jsonify(user.serialize()), 200
+
+@api.route('/users/me',mehods=['PUT'])
+@jwt_required()
+def update_profile():
+    user_id = get_jwt_identity()
+    data = request.get_json() or{}
+    user = User.query.get(user_id)
+    if not user:
+        raise APIException("Usuario no encontrado", status_code=404)
+    
+    if "name" in data:
+        user.name = data["name"]
+    if "level" in data:
+        user.level = data["level"]
+
+    db.session.commit()
+    return jsonify({"Perfil actualizado"}), 200
+
+@api.route('/users/me', methods=['delete'])
+@jwt_required()
+def delete_account():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    if not user:
+        raise APIException("Usuario no encontrado", status_code=404)
+    
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"msg":  "Cuenta eliminada"}), 200
+
+# CRUD de Eventos y endpoint de “unirse” a un evento
+
+@api.route('/events', methods=['Get'])
+def list_events():
+    events = Event.query.all()
+    return jsonify([e.serialize() for e in events]), 200
+
+@api.route('/events/<int:event_id>', methods=['GET'])
+def get_event(event_id):
+    event = Event.query.get(event_id)
+    if not event:
+        raise APIException("Evento no encontrado", status_code=404)
+    return jsonify(event.serialize()), 200
+
+@api.route('/events', methods=['POST'])
+@jwt_required()
+def create_event():
+    data = request.get_json() or {}
+    required = ["sport", "datetime", "lat", "lng", "capacity", "price"]
+    if not all(field in data for field in required):
+        raise APIException("Faltan campos obligatorios", status_code=400)
+    
+    event = Event(
+        sport=data["sport"],
+        datetime=data["datetime"],
+        lat=float(data["lat"]),
+        lng=float(data["lng"]),
+        capacity=int(data["capacity"]),
+        price=int(data["price"]),
+        is_free=(int(data["price"]) == 0)
+    )
+
+    db.session.add(event)
+    db.session.commit()
+    return jsonify(event.serialize()), 201
+
+@api.route('/events/<int:event_id>', methods=['PUT'])
+@jwt_required()
+def update_event(event_id):
+
+    event = Event.query.get(event_id)
+    if not event:
+        raise APIException("Evento no encontrado", status=404)
+    
+    data = request.get.json() or {}
+    
+    for field in ["sport", "datetime", "lat", "lng", "capacity", "price"]:
+        if field in data:
+            setattr(event, field, type(getattr(event, field))(data[field]))
+            
+        event.is_free = (event.price == 0)
+
+        db.session.commit()
+        return jsonify(event.serialize()), 200
+    
+@api.route('/events/<int:event_id>',methods=['DELETE'])
+@jwt_required()
+def delete_event(event_id):
+
+    event = Event.query.get(event_id)
+    if not event:
+        raise APIException("Evento no encontrado", status_code=404)
+    
+    db.session.delete(event)
+    db.session.commit()
+    return jsonify({"msg": "Evento eliminado"}), 200
+
+@api.route('/events/<int:event_id>/join', methods=['POST'])
+@jwt_required()
+def join_event(event_id):
+
+    user_id = get_jwt_identity()
+    event = Event.query.get(event_id)
+    if not event:
+        raise APIException("Evento no encontrado", status_code=404)
+    
+    if len(event.players) >=event.capacity:
+        raise APIException("Evento lleno", status_code=400)
+    
+    exist = EventPlayer.query.filter_by(user_id=user_id, event_id=event_id).first()
+    if exist:
+        raise APIException("Ya inscrito en este evento", status_code=409)
+    
+    inscription = EventPlayer(
+        user_id=user_id,
+        event_id=event_id,
+        paid=event.is_free
+    )
+    db.session.add(inscription)
+    db.session.commit()
+    return jsonify(inscription.serialize()), 201

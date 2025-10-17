d4feab9b583603f19c93ce6bd2ee14dc79c1014f
CarlosAguayo1 <carlosibanezaguayo@gmail.com>
2025-10-17 11:29:47 +0000

    proyecto final

--- src/app.py
+++ src/app.py
@@ -3,9 +3,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 from dotenv import load_dotenv
 import os
-
-load_dotenv()
-
+from flask import Flask, jsonify, send_from_directory
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
@@ -16,13 +14,17 @@ from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_jwt_extended import JWTManager, get_jwt_identity
 
+load_dotenv()
+
 
 # from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dist')
-app = Flask(__name__)
+app = Flask(__name__, static_folder='dist', static_url_path='/')
 app.url_map.strict_slashes = False
+
+print("🗂 STATIC FOLDER:", os.path.join(os.path.dirname(__file__), 'dist'))
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -67,20 +69,18 @@ def handle_invalid_usage(error):
 
 
 @app.route('/')
-def sitemap():
-    if ENV == "development":
-        return generate_sitemap(app)
-    return send_from_directory(static_file_dir, 'index.html')
+def index():
+    return app.send_static_file('index.html')
 
 # any other endpoint will try to serve it like a static file
-@app.route('/<path:path>', methods=['GET'])
-def serve_any_other_file(path):
-    if not os.path.isfile(os.path.join(static_file_dir, path)):
-        path = 'index.html'
-    response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0  # avoid cache memory
-    return response
-
+@app.route('/<path:path>')
+def spa_fallback(path):
+    # Si existe el fichero pedido en dist, entrégalo
+    full_path = os.path.join(app.static_folder, path)
+    if os.path.exists(full_path):
+        return send_from_directory(app.static_folder, path)
+    # Si no, devuelve index.html
+    return app.send_static_file('index.html')
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
